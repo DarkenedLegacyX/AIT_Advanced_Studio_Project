ffd66e29a01ecb21c87e8ff9408708a8888f1fd0
DarkenedLegacyX <7092@ait.nsw.edu.au>
2021-04-19 02:33:18 +1000

    edited images and cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,40 +5,40 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Check out what the app's features and benefits!'</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              src='images/ReduceWaste.jpg'
+              text='Keep track of your products and know when they will expire'
+              label='Reduce Waste'
               //path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              src='images/Ingredient.jpg'
+              text='Know how much ingredients you will need on your next order'
+              label='Predict Stock'
               //path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              src='images/Ordering.jpg'
+              text='Be able to place orders in the system'
+              label='Ordering System'
               //path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              src='images/Reliability.jpg'
+              text='”Honesty is the first chapter in the book of wisdom.” – Thomas Jefferson'
+              label='Reliability'
               //path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
+              src='images/Simple.jpg'
+              text='Simple and easy to learn'
+              label='Easy to Use'
               //path='/sign-up'
             />
           </ul>
